3b4ceb2a30f87c12b885dab43e44847307091313
叶璨铭 <12011404@mail.sustech.edu.cn>
2022-04-12 21:17:49 +0800

    feat(divide and conquer): binary_search_for_first_satisfies

--- src/main/cpp/cn/edu/SUSTech/YeCanming/Algs/DivideAndConquer/BinarySearch.hpp
+++ src/main/cpp/cn/edu/SUSTech/YeCanming/Algs/DivideAndConquer/BinarySearch.hpp
@@ -21,9 +21,9 @@ namespace cn::edu::SUSTech::YeCanming::Algs::DivideAndConquer {
      * Warning:
      *      If the range is not monotonically decreasing according to the predicate,
      *      then the behavior is undefined.
-     *      In other words, the satisfaction sequence must be monotonically non-increasing.
+     *      In other words, the satisfaction sequence must be monotonically non-increasing(ones then zeros).
      * SpecialCase:
-     *      If the sequence is 0000...0, then the result is first-1;
+     *      If the sequence is 0000...0(all are zero), then the result is std::end(your container), namely parameter "last";
      * Time complexity: If std::distance(first, last) is n, then the time complexity is O(log n)
      */
     template<class It, class Pr>
@@ -34,12 +34,46 @@ namespace cn::edu::SUSTech::YeCanming::Algs::DivideAndConquer {
             if (pred(*mid)){
                 left = mid + 1; //If it satisfies, try to find a better match.
             } else{
-                right = mid; //If it doesn't, we are now in the 0 section, let's try to look at
+                right = mid; //If it doesn't, we are now in the 0 section, let's try to look to our left.
             }
         }
         assert (std::distance(left, right)==0);
         if (left==first)
             return last; //全0的情况。如果first的左边有哨兵，那么是left-1，可惜C++管理左闭右开。
         return left-1;//减1是因为 left 是激进指针，最后收敛的时候和 right 相等，但是 right 是一定不对的答案, 所以结果必然是left-1。
+    }
+    /**
+     * General purpose binary search algorithm, also known as "二分答案"(binary search for answer).
+     * @tparam It
+     * @tparam Pr
+     * @param first
+     * @param last
+     * @param pred
+     * @return the first element in the range that satisfies the predicate.
+     * Behavior:
+     *      Define the satisfaction sequence to be s, such that s[i] = pred(first[i]) .
+     *      For example, the satisfaction sequence is 0000001111111.
+     *      Then this algorithm will find the first 1('s iterator), which is after the last 0.
+     * Warning:
+     *      If the range is not monotonically increasing according to the predicate,
+     *      then the behavior is undefined.
+     *      In other words, the satisfaction sequence must be monotonically non-increasing(zeros then ones).
+     * SpecialCase:
+     *      If the sequence is 0000...0(all are zero), then the result is std::end(your container), namely parameter "last";
+     * Time complexity: If std::distance(first, last) is n, then the time complexity is O(log n)
+     */
+    template<class It, class Pr>
+    It binary_search_for_first_satisfies(const It first, const It last, const Pr& pred){
+        It left = first, right = last;
+        while (std::distance(left, right)>0){ //当大于0的时候, 还有元素需要搜索。
+            It mid = left + (std::distance(left, right)>>1);
+            if (pred(*mid)){
+                right = mid; //If it satisfies, try to find a better match.
+            } else{
+                left = mid + 1; //If it doesn't, we are now in the 0 section, let's try to look to our right.
+            }
+        }
+        assert (std::distance(left, right)==0);
+        return left; //left 为保守指针，一定为正确答案（或者到达了last）。
     }
 }
--- src/test/cpp/cn/edu/SUSTech/YeCanming/Algs/DivideAndConquer/GTestBinarySearch.cpp
+++ src/test/cpp/cn/edu/SUSTech/YeCanming/Algs/DivideAndConquer/GTestBinarySearch.cpp
@@ -12,6 +12,7 @@ namespace cn::edu::SUSTech::YeCanming::Algs::DivideAndConquer {
     TEST(GTestBinarySearch, UsedToFindElement){
         std::vector<int> a(10);
         std::iota(a.begin(), a.end(), 0); //0,1,2,...,9
+      // binary_search_for_last_satisfies
         auto result = ThisPackage::binary_search_for_last_satisfies(a.begin(), a.end(), [](const int& a){
             return a<=5;
         });
@@ -20,15 +21,34 @@ namespace cn::edu::SUSTech::YeCanming::Algs::DivideAndConquer {
             return a<=5;
         });
         EXPECT_EQ(*resultC, 5);
+        // binary_search_for_first_satisfies
+        result = ThisPackage::binary_search_for_first_satisfies(a.begin(), a.end(), [](const int& a){
+            return a>=5;
+        });
+        EXPECT_EQ(*result, 5);
+        resultC = ThisPackage::binary_search_for_first_satisfies(a.cbegin(), a.cend(), [](const int& a){
+            return a>=5;
+        });
+        EXPECT_EQ(*resultC, 5);
     }
     TEST(GTestBinarySearch, WhenSatifactionSequenceAreAllZero){
         std::vector<int> a(10);
         std::fill(a.begin(), a.end(), 10);
+        // binary_search_for_last_satisfies
         auto result = ThisPackage::binary_search_for_last_satisfies(a.begin(), a.end(), [](const int& a){
             return a>11;
         });
         EXPECT_EQ(result, a.end());
         auto resultC = ThisPackage::binary_search_for_last_satisfies(a.cbegin(), a.cend(), [](const int& a){
+            return a>11;
+        });
+        EXPECT_EQ(result, a.cend());
+        // binary_search_for_first_satisfies
+        result = ThisPackage::binary_search_for_first_satisfies(a.begin(), a.end(), [](const int& a){
+            return a>11;
+        });
+        EXPECT_EQ(result, a.end());
+        resultC = ThisPackage::binary_search_for_first_satisfies(a.cbegin(), a.cend(), [](const int& a){
             return a>11;
         });
         EXPECT_EQ(result, a.cend());
