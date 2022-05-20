03fd962b65b17ef60982c80bd66ab23c5042f075
叶璨铭 <12011404@mail.sustech.edu.cn>
2022-05-20 18:14:28 +0800

    feat(Lab10): 100节点的图会被卡住tle

--- src/main/cpp/cn/edu/SUSTech/YeCanming/Algs/lab11_network_flow/problemA.cpp
+++ src/main/cpp/cn/edu/SUSTech/YeCanming/Algs/lab11_network_flow/problemA.cpp
@@ -8,6 +8,7 @@
 #include <queue>
 #include <cstdint>
 #include <limits>
+#include <cassert>
 #define Square(X) ((X)*(X))
 
 template<class Capacity = size_t>
@@ -30,10 +31,10 @@ public:
     }
     Capacity getMaximumFlow(){
         Capacity maximumFlow{0};
-//        Capacity maximumFlow{std::numeric_limits<Capacity>::min()};
         auto residualGraph = mAdjacencyMatrix;
         auto allowCapacity = std::numeric_limits<Capacity>::max()>>31;
-        for (bool cannotFindPath = true; allowCapacity>0; allowCapacity>>=cannotFindPath){ //如果找不到路径，就降低要求。比如如果要求是1，一直找得到，就不会降，最后找不到了变成0，算法结束。
+        for (; allowCapacity>0; ){
+            bool cannotFindPath = true;
             //对 residualGraph 做一次bfs，找到src到sink路径的同时
             // 1. 确保路径流量大于等于 allowCapacity
             // 2. 修改 residualGraph
@@ -42,18 +43,35 @@ public:
             std::vector<bool> isVisited(vertexCnt+1);
             std::vector<size_t> parent(vertexCnt+1);
             toBeVisit.push(source);
-            while (!toBeVisit.empty()){
-                auto top = toBeVisit.front(); toBeVisit.pop(); isVisited[top] = true;
+            isVisited[source] = true;
+            while (!toBeVisit.empty()){ //如果是从while的条件break的，就是没找到合适的路径，但是找到了所有source可达点的最短路径。
+                auto top = toBeVisit.front(); toBeVisit.pop();
                 if (top==sink){
                     cannotFindPath = false;
-                    break;
+                    break; //代表找到了最短路径。
                 }
                 for (int j = 1; j <= vertexCnt; ++j) { //遍历连接的所有边。
                     if (!isVisited[j] && CapacityFromIToJ(residualGraph, top, j)>=allowCapacity){
+                        isVisited[top] = true;//写在这里而不是pop后面，这样就可以防止后面队列太大。
+                        parent[j] = top; //上面那个isVisited很重要，保证了parent不会被人乱改。parent始终是记录最短路径的。
                         toBeVisit.push(j);
-                        parent[j] = top;
                     }
                 }
+            }
+            if (!cannotFindPath){
+                Capacity bottleNeck{std::numeric_limits<Capacity>::max()};
+                for (size_t it=sink; it!=source; it = parent[it]){
+                    assert(it!=0);
+                    bottleNeck = std::min(bottleNeck, CapacityFromIToJ(residualGraph, parent[it], it));
+                }
+                for (size_t it=sink; it!=source; it = parent[it]){
+                    assert(it!=0);
+                    CapacityFromIToJ(residualGraph, parent[it], it)-=bottleNeck;
+                    CapacityFromIToJ(residualGraph, it, parent[it])+=bottleNeck;
+                }
+                maximumFlow+=bottleNeck;
+            }else{
+                allowCapacity>>=cannotFindPath; //如果找不到路径，就降低要求。比如如果要求是1，一直找得到，就不会降，最后找不到了变成0，算法结束。
             }
         }
         return maximumFlow;
