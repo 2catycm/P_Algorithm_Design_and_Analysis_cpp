3b48be6ee0a567c00d0b108925b4e51c9028f39e
叶璨铭 <12011404@mail.sustech.edu.cn>
2022-05-22 23:34:11 +0800

    feat(Lab11): Dinic算法可以过A题，过不了洛谷

--- src/main/cpp/cn/edu/SUSTech/YeCanming/Algs/lab11_network_flow/DinicFlowNetwork.h
+++ src/main/cpp/cn/edu/SUSTech/YeCanming/Algs/lab11_network_flow/DinicFlowNetwork.h
@@ -27,7 +27,7 @@ public:
     size_t source, sink;
     std::vector<size_t> depth; //表示bfs的层级
     explicit FlowNetwork(size_t vertexCnt, size_t edgeCnt=0, size_t source=0, size_t sink=0)
-        : vertexCnt(vertexCnt), edgeCnt(edgeCnt), source(source), sink(sink), mAdjacencyList(vertexCnt + 1), depth(vertexCnt+1), mIsVisited(vertexCnt+1) {
+        : vertexCnt(vertexCnt), edgeCnt(edgeCnt), source(source), sink(sink), mAdjacencyList(vertexCnt + 1), depth(vertexCnt+1), mValidRoute(vertexCnt+1) {
         mEdges.reserve(edgeCnt);
     }
     void addEdge(size_t vertexU, size_t vertexV, Capacity capacity){
@@ -41,7 +41,7 @@ public:
         Capacity maximumFlow{0};
         while (bfs()){
             MyLog<<"\tThere is an augmenting route."<<std::endl;
-            std::fill(mIsVisited.begin(), mIsVisited.end(), false);
+            std::fill(mValidRoute.begin(), mValidRoute.end(), 0);
             MyLog<<"\tDoing dfs."<<std::endl;
             maximumFlow += dfs(source, CapacityInf);
         }
@@ -49,28 +49,29 @@ public:
     }
 
 private:
-    std::vector<bool> mIsVisited; // 表示"当前弧优化" 的边的索引。
+    std::vector<size_t> mValidRoute; // 表示"当前弧优化" 的边的索引。
     /**
      * build or rebuild the depth array.
      * @return whether there is an augmenting path.
      */
     bool bfs(Capacity allowCapacity = 0){
         MyLog<<"\tDoing bfs on the residual graph(denoted by flow on edge)."<<std::endl;
+        std::vector<bool> isVisited(vertexCnt+1);
         std::queue<size_t> toBeVisited;
         toBeVisited.emplace(source);
-        mIsVisited[source] = true;
+        isVisited[source] = true;
         while (!toBeVisited.empty()){
             auto top = toBeVisited.front();
             toBeVisited.pop();
             for (const auto& _edge:mAdjacencyList[top]){
                 auto& edge = mEdges[_edge];
-                if (!mIsVisited[edge.to] && edge.cap-edge.flow>allowCapacity){
-                    mIsVisited[edge.to] = true;
+                if (!isVisited[edge.to] && edge.cap-edge.flow>allowCapacity){
+                    isVisited[edge.to] = true;
                     depth[edge.to] = depth[top]+1;
                     toBeVisited.emplace(edge.to);
                 }
             }
-            if (mIsVisited[sink])
+            if (isVisited[sink])
                 return true;
         }
         return false;
@@ -87,11 +88,11 @@ private:
             return allowCapacity; //如果要递归访问sink，那么保持原有capacity不变；如果原来没有可行capacity，返回0.
         Capacity maximumFlowFromCurrentNode = 0;
         auto& relatives = mAdjacencyList[currentNode];
-//        for (size_t& i = mValidRoute[currentNode]; i < relatives.size(); ++i){
-//            size_t edgeIndex = relatives[i];
-        for (const auto& edgeIndex:relatives){
+        for (size_t& i = mValidRoute[currentNode]; i < relatives.size(); ++i){ //注意这个引用，它让我们下次dfs的时候不忘过往。
+            size_t edgeIndex = relatives[i];
+//        for (const auto& edgeIndex:relatives){
             Edge& edge = mEdges[edgeIndex]; // 这是我们将要走过的边。
-            if (depth[currentNode]+1==depth[edge.to] && !mIsVisited[edge.to]){
+            if (depth[currentNode]+1==depth[edge.to]){
                 //按照bfs的指导，确定我们要遍历的边。
                 // 由于每次都要访问新的一层，所以一定不会访问到访问过的。
                 auto bottleNeck = dfs(edge.to, std::min(allowCapacity, edge.cap-edge.flow)); // 我们要走的边能达到的最大流是多少？被前面的allowCapacity限制，也被这条边限制，其他的问题递归决定。
@@ -104,12 +105,13 @@ private:
 //                   mValidRoute+=
                    if (allowCapacity<=0){
                        assert(allowCapacity==0);
-                       break;
+                       break; //来路没有容量了，于是下面的边怀才不遇。
+                       //但是当这一次dfs浪潮退去，下一次dfs重新到来，我们的容量再次回来。
+                       //此时我们只需从上一次来还没用的边继续增广。
                    }
                 }
             }
         }
-        mIsVisited[currentNode] = true; //退栈。
         MyLog<<"\t\tThe maximumFlowFrom "<<currentNode<<" is "<<maximumFlowFromCurrentNode<<std::endl;
         return maximumFlowFromCurrentNode;
     }
